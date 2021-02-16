26edcca3f137ca389281de12fab24079b80571a5
CodeDoctorDE <wusel905@gmail.com>
2021-02-16 17:12:29 +0100

    change build script

--- tools/build.py
+++ tools/build.py
@@ -6,17 +6,15 @@ users = os.listdir('metadata')
 print("Building data files...")
 users = list(filter(lambda user: os.path.isdir(os.path.join('metadata', user)), users))
 for user in users:
-    entries = []
-    files = []
+    entries = {}
     current = os.path.join('metadata', user)
     for item in os.listdir(current):
         if(item != "data.json"):
             with open(os.path.join(current, item), "r") as f:
                 data = json.load(f)
-                files.append(os.path.splitext(item)[0])
-                entries.append(data['url'])
+                entries[os.path.splitext(item)[0]] = data['url']
     with open(os.path.join(current, "data.json"), "w") as f:
-        json.dump({'entries': entries, 'files': files}, f, ensure_ascii=False, indent=4)
+        json.dump({'entries': entries}, f, ensure_ascii=False, indent=4)
     print(user)
 with open(os.path.join('metadata', 'data.json'), "w") as f:
     json.dump(users, f, ensure_ascii=False, indent=4)
